33a5eaea4087844f5c1d25adedcbb4a762894ab9
alielmouedden <alielmouedden9@gmail.com>
2025-01-13 05:10:05 +0100

    d

--- api/index.php
+++ api/index.php
@@ -1,17 +1,4 @@
+// api/index.php
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-
-use Illuminate\Foundation\Application;
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
